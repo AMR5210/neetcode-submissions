2fcc279cf13e70c29e739eb55fcffca8097cfe31
Akshay Madavalappil Ramesh <221825473+AMR5210@users.noreply.github.com>
2026-06-28 15:18:28 +0530

    Add: string-encode-and-decode - submission-0

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-0.py
@@ -0,0 +1,20 @@
+from typing import List
+
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # word is preceded by its length and '#'
+        return "".join(f"{len(s)}#{s}" for s in strs)
+
+    def decode(self, s: str) -> List[str]:
+        res = []
+        i = 0
+        
+        while i < len(s):
+            j = s.find('#', i)
+            length = int(s[i:j])
+            word = s[j + 1 : j + 1 + length]
+            res.append(word)
+            i = j + 1 + length
+            
+        return res
